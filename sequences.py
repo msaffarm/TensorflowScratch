1f60a55617ea8927984be91e006f487fa08f1f93
mansour <msaffarmehrjardy@gmail.com>
2017-01-25 23:36:13 -0700

    sequences in tensorflow

--- sequences.py
+++ sequences.py
@@ -1,1 +1,9 @@
 import tensorflow as tf
+
+# 2 simple kind of sequences in tf
+a = tf.linspace(10.,100.,10) # start and stop in {float32,float64}, num in {int32,int64}
+b = tf.range(10.)
+c = tf.range(1,10,2)
+
+with tf.Session() as sess:
+	print(sess.run([a,b,c]))
